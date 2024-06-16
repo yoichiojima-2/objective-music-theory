79ca723311f63d49aca11ce77651b00a084bc2c9
Yoichi Ojima <yoichiojima@gmail.com>
2024-06-16 19:31:26 +0900

    add __getitem__ to CircleOfFifth

--- circle_of_fifth.py
+++ circle_of_fifth.py
@@ -4,15 +4,20 @@ from pitch import Pitch
 
 class CircleOfFifth:
     def __init__(self, root: Pitch):
-        self.root = root
-        self.pitches = [Pitch(root.number + i * 7) for i in range(12)]
+        self.root: Pitch = root
+        self.pitches: list[Pitch] = [Pitch(root.number + i * 7) for i in range(12)]
+
+    def __getitem__(self, index: int) -> Pitch:
+        return self.pitches[index]
 
 
 def test_circle_of_fifth():
     c = CircleOfFifth(Pitch("c"))
     print([n.name for n in c.pitches])
     assert [n.name for n in c.pitches] == ["c", "g", "d", "a", "e", "b", "f#", "c#", "g#", "d#", "a#", "f"]
+    assert c[1].name == "g"
 
     a = CircleOfFifth(Pitch("a"))
     print([n.name for n in a.pitches])
     assert [n.name for n in a.pitches] == ["a", "e", "b", "f#", "c#", "g#", "d#", "a#", "f", "c", "g", "d"]
+    assert a[2].name == "b"
