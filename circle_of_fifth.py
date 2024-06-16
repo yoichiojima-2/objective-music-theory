058b09a0bd29c5c818587098a3abb08a1b1aa627
Yoichi Ojima <yoichiojima@gmail.com>
2024-06-16 10:57:34 +0900

    update circle of fifth

--- circle_of_fifth.py
+++ circle_of_fifth.py
@@ -2,25 +2,17 @@ from __future__ import annotations
 from pitch import Pitch
 
 
-class CircleOfFifths:
-    def __init__(self, pitch: Pitch):
-        self.pitch = pitch
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        self.pitch = Pitch(self.pitch.number + 7)
-        return self.pitch
+class CircleOfFifth:
+    def __init__(self, root: Pitch):
+        self.root = root
+        self.pitches = [Pitch(root.number + i * 7) for i in range(12)]
 
 
-def test_circle_of_fifths():
-    circle = CircleOfFifths(Pitch("c"))
-    assert circle.pitch.name == "c"
-    print(circle.pitch)
-    next(circle)
-    print(circle.pitch)
-    assert circle.pitch.name == "g"
-    next(circle)
-    print(circle.pitch)
-    assert circle.pitch.name == "d"
+def test_circle_of_fifth():
+    c = CircleOfFifth(Pitch("c"))
+    print([n.name for n in c.pitches])
+    assert [n.name for n in c.pitches] == ["c", "g", "d", "a", "e", "b", "f#", "c#", "g#", "d#", "a#", "f"]
+
+    a = CircleOfFifth(Pitch("a"))
+    print([n.name for n in a.pitches])
+    assert [n.name for n in a.pitches] == ["a", "e", "b", "f#", "c#", "g#", "d#", "a#", "f", "c", "g", "d"]
