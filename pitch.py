99d09275a8d7188c49acfdaafefa6cb6d5165c36
Yoichi Ojima <yoichiojima@gmail.com>
2024-06-17 23:27:30 +0900

    add octave attribute

--- pitch.py
+++ pitch.py
@@ -1,25 +1,36 @@
+from __future__ import annotations
 from dataclasses import dataclass
 
 
 @dataclass
 class Pitch:
     number: int
+    octave: int = 0
 
-    def rel(self, shift):
-        return Pitch(self.number + shift)
+    def rel(self, shift) -> Pitch:
+        shifted_num = self.number + shift
+        return Pitch(
+            number = shifted_num % 12,
+            octave = self.octave + (shifted_num // 12)
+        )
 
-    def round(self):
-        pitch = self.number
-        while pitch <= 0:
-            pitch += 12
-        return Pitch(pitch % 12)
+    def round(self) -> Pitch:
+        self.ocvate = 0
+        return self
 
 
 def test_pitch():
     p = Pitch(0)
     assert p.number == 0
-    print(p.number)
+    assert p.octave == 0
+    print(p)
 
     p2 = p.rel(7)
     assert p2.number == 7
-    print(p2.number)
+    assert p2.octave == 0
+    print(p2)
+
+    p3 = p2.rel(7)
+    assert p3.number == 2
+    assert p3.octave == 1
+    print(p3)
