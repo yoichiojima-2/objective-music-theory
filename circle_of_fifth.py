e5db794e3f12fa7aa8d544a519ce6e2776a6a38d
yo <145314115+yoichiojima-2@users.noreply.github.com>
2024-06-24 12:25:39 +0000

    add shift method

--- circle_of_fifth.py
+++ circle_of_fifth.py
@@ -1,5 +1,6 @@
-from __future__ import annotation
+from __future__ import annotations
 from dataclasses import dataclass
+from pitch import Pitch
 
 
 @dataclass
@@ -7,12 +8,17 @@ class CircleOfFifth:
     cursor: Pitch
 
     def __init__(self, cursor: Pitch):
-        self.curosr = Pitch
-        
-    def next(self) -> CIrcleOfFifth:
-        self.cursor = self.cursor.rel(7)
+        self.cursor = cursor
+
+    def shift(self, n) -> CircleOfFifth:
+        self.cursor = self.cursor.rel(n * 7)
         return self
 
-    def prev(self) -> CIrcleOfFifth:
-        self.cursor = self.cursor.rel(-7)
-        return self
+
+def test_circle_of_fifth():
+    cof = CircleOfFifth(Pitch(0))
+    assert cof.cursor.number == 0
+    cof.shift(2)
+    assert cof.cursor.number == 14
+    cof.shift(-3)
+    assert cof.cursor.number == -7
