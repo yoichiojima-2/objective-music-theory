c35f966df61093807bcef562bd74da7f5e427b56
Yoichi Ojima <yoichiojima@gmail.com>
2024-06-30 14:42:15 +0900

    add constructor to pitch, add sort, reset octave method to pitches

--- pitch.py
+++ pitch.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from dataclasses import dataclass
 import itertools
+import typing as t
 
 
 @dataclass
@@ -13,6 +14,32 @@ class Pitch:
             number = (self.number + n) % 12,
             octave = self.octave + (self.number + n) // 12
         )
+
+    @staticmethod
+    def new_from_str(s) -> Pitch:
+        match s:
+            case "C":
+                return Pitch(0)
+            case "C#" | "Db":
+                return Pitch(1)
+            case "D":
+                return Pitch(2)
+            case "D#" | "Eb":
+                return Pitch(3)
+            case "E":
+                return Pitch(4)
+            case "F":
+                return Pitch(5)
+            case "F#" | "Gb":
+                return Pitch(6)
+            case "G":
+                return Pitch(7)
+            case "G#" | "Ab":
+                return Pitch(8)
+            case "A":
+                return Pitch(9)
+            case "A#" | "Bb":
+                return Pitch(10)
 
 
 class Pitches:
@@ -28,7 +55,7 @@ class Pitches:
     def __next__(self) -> Pitch:
         return next(self._iter)
 
-    def cycle(self, n) -> Pitch:
+    def cycle(self, n) -> t.Iterable[Pitch]:
         cnt = 0
         for i in itertools.cycle(self.pitches):
             if cnt == n:
@@ -36,6 +63,14 @@ class Pitches:
             cnt += 1
             yield i
 
+    def reset_octave(self) -> Pitches:
+        for p in self.pitches:
+            p.octave = 0
+        return self
+
+    def sort(self) -> Pitches:
+        self.pitches.sort(key=lambda p: (p.number, p.octave))
+        return self
 
 def test_pitch():
     p = Pitch(0)
@@ -54,6 +89,14 @@ def test_pitch():
     assert p4.number == 5
     assert p4.octave == -1
 
+    p5 = Pitch.new_from_str("C")
+    assert p5.number == 0
+
+    p6 = Pitch.new_from_str("C#")
+    assert p6.number == 1
+
+
+def test_pitches():
     ps = Pitches([Pitch(0), Pitch(1), Pitch(2)])
     ps.pitches[0].number == 0
     ps.pitches[1].number == 1
