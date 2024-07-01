60a5d588c462cef74cf58836ffad18333dc143cb
Yoichi Ojima <yoichiojima@gmail.com>
2024-07-01 23:12:39 +0900

    impl with generators

--- scale.py
+++ scale.py
@@ -4,23 +4,38 @@ from circle_of_fifth import CircleOfFifth
 
 
 class Scale:
-    @property
-    def build(root: Pitch, feeling: int = 0, length: int = 7) -> Pitches:
+    @staticmethod
+    def generate(root: Pitch, feeling: int) -> Pitches:
+        'ionian will be returned when called with feeling = 0'
         cof = CircleOfFifth(root)
+        cof.shift(feeling - 2)
+        while True:
+            yield cof.shift(1).cursor
 
-        scale = []
+    @classmethod
+    def generate_up_to_n(cls, root: Pitch, feeling: int, n: int) -> Pitches:
+        cof = CircleOfFifth(root)
         cof.shift(feeling - 1)
-        for _ in range(length):
-            scale.append(cof.shift(1).cursor)
 
-        return Pitches(scale).reset_octave().sort()
+        cnt = 0
+        for p in cls.generate(root, feeling):
+            if cnt > (n - 1):
+                break
+            yield p
+            cnt += 1
 
 
 def test_scale():
-    c_ionian = Scale.build(Pitch.new_from_str("C"), 0, 7)
-    c_ionian_nums = [p.number for p in c_ionian.pitches]
-    assert c_ionian_nums == [0, 2, 4, 5, 7, 9, 11]
+    c_ionian = (
+        Pitches(list(Scale.generate_up_to_n(Pitch.new_from_str("C"), 0, 7)))
+        .sort()
+        .reset_octave()
+    )
+    assert c_ionian.numbers == [0, 2, 4, 5, 7, 9, 11]
 
-    c_aeolian = Scale.build(Pitch.new_from_str("C"), -3, 7)
-    c_aeolian_nums = [p.number for p in c_aeolian.pitches]
-    assert c_aeolian_nums == [0, 2, 3, 5, 7, 8, 10]
+    c_aeolian = (
+        Pitches(list(Scale.generate_up_to_n(Pitch.new_from_str("C"), -3, 7)))
+        .sort()
+        .reset_octave()
+    )
+    assert c_aeolian.numbers == [0, 2, 3, 5, 7, 8, 10]
